9ddebf4e65c376025b67b3167f6c3fcc555d22aa
EdWinMaster0 <124351135+EdWinMaster0@users.noreply.github.com>
2025-01-11 18:02:08 +0100

    Add files via upload
    snake still working

--- snake-survivors/scenes/player_movement.gd
+++ snake-survivors/scenes/player_movement.gd
@@ -1,20 +1,19 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var num_segments = 3
+@export var num_segments = 10
 @export var delay_frames = 12  # speed 200 => 24, speed 400=> 12
 var segments = []
 var position_history = []
 var dir_history = []
 var segment_scene = preload("res://scenes/Segment.tscn")
-var s
 func _ready():
 	for i in range(num_segments):
-		s = segment_scene.instantiate()
+		var s = segment_scene.instantiate()
 		segments.append(s)
+		s.z_index = num_segments-i
 		call_deferred("add_sibling", s)
-	for i in range(segments.size()):
-		print("asd:",segments[i], i)
+	z_index = num_segments+1
 
 var dir
 func get_input():
@@ -26,15 +25,10 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide()
 	
-	if position_history.size() <= delay_frames:
-		pass
-
-	# Check if player position changed and store the new position
-	if position != position_history.back():  # Only update if position has changed
+	if position != position_history.back():
 		position_history.append(position)
 		dir_history.append(dir)
 
-	# Keep only the last `delay_frames` positions in history
 	if position_history.size() > delay_frames * (num_segments + 1):
 		position_history.pop_front()
 		dir_history.pop_front()
@@ -44,9 +38,9 @@ func _physics_process(delta):
 			segments[i].position = position_history[(num_segments-i)*delay_frames]
 			segments[i].rotation = dir_history[(num_segments-i)*delay_frames].angle()
 		elif position_history.size() > (i+1)*delay_frames:
-			segments[i].position = position_history[(position_history.size()%delay_frames)]
-			segments[i].rotation = dir_history[position_history.size()%delay_frames].angle()
+			segments[i].position = position_history[position_history.size()-(i+1)*delay_frames]
+			segments[i].rotation = dir_history[position_history.size()-(i+1)*delay_frames].angle()
 			
 		
-	if dir.length() > 0:  # Ensure there's movement
+	if dir.length() > 0:
 		rotation = dir.angle()
