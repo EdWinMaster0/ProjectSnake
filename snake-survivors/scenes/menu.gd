442940cff55ca4e87b77eaa4518d97b43277d1b9
blint500 <124351711+MotherFCKR54@users.noreply.github.com>
2025-01-25 00:04:45 +0100

    Menu update
    Options button, Start Button, Exit button, Background

--- snake-survivors/scenes/menu.gd
+++ snake-survivors/scenes/menu.gd
@@ -2,15 +2,16 @@ extends Control
 
 
 
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Map1.tscn")
-
-
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 
+func _on_pressed() -> void:
+	GlobalVariables._reset()
+	get_tree().change_scene_to_file("res://scenes/Map1.tscn")
 
-func _on_quit_pressed() -> void:
+
+
+func _on_texture_button_pressed() -> void:
 	get_tree().quit()
